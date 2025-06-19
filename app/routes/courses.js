08c9506c17eecfe27e2eebd0320d7459ac023b16
wael <waeletta4@gmail.com>
2025-06-19 11:27:38 +0300

    chatGPT reorder course apis,

--- app/routes/courses.js
+++ app/routes/courses.js
@@ -18,42 +18,25 @@ const {
 } = require("../controllers/courseCont");
 const { admin, teacher, authentication } = require("../middlewares/authentication");
 
+// Authenticated user routes
 router.use(authentication);
 
-router.route("/new-courses")
-    .get(newCourses);
-router.route("/on-sales-courses")
-    .get(onSaleCourses);
-router.route("/popular-courses")
-    .get(popularCourses);
-router.route("/purchased-courses")
-    .get(getAllPurchasedCourses);
+router.route("/new-courses").get(newCourses);
+router.route("/on-sales-courses").get(onSaleCourses);
+router.route("/popular-courses").get(popularCourses);
+router.route("/purchased-courses").get(getAllPurchasedCourses);
+router.route("/search").get(searchCourse);
+router.route("/all-courses-revenue").get(getAllCoursesRevenue); // Moved up
+router.route("/course-revenue/:id").get(getCourseRevenue); // Moved up
+router.route("/").get(getAllCourses);
+router.route("/:id").get(getCourseById);
 
-router.route("/search")
-    .get(searchCourse);
-router.route("/")
-    .get(getAllCourses)
-
-router.route("/:id")
-    .get(getCourseById)
-
-
+// Admin-only routes
 router.use(admin);
 
-router.route("/show-hide-course/:id")
-    .put(showOrHideCourse);
-router.route("/course-revenue/:id")
-    .get(getCourseRevenue);
-router.route("/all-courses-revenue")
-    .get(getAllCoursesRevenue);
-router.route("/add")
-    .post(createCourse);
-
-router.route("/:id")
-    .put(updateCourse)
-    .delete(deleteCourse);
-
-router.route("/")
-    .delete(deleteAllCourses);
+router.route("/show-hide-course/:id").put(showOrHideCourse);
+router.route("/add").post(createCourse);
+router.route("/:id").put(updateCourse).delete(deleteCourse);
+router.route("/").delete(deleteAllCourses);
 
 module.exports = router;
